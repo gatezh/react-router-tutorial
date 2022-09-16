13e27451813ab448a60c64f1553838f391b9aeff
Sergii Gatezh <gatezh@users.noreply.github.com>
2022-09-16 14:24:41 -0400

    Save contact data updates

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Root, {
   action as rootAction,
 } from "./routes/root";
 import Contact, { loader as contactLoader } from "./routes/contact";
-import EditContact from "./routes/edit";
+import EditContact, { action as editAction } from "./routes/edit";
 import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
@@ -28,6 +28,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
+        action: editAction,
       },
     ],
   },
