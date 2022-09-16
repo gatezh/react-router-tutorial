88f4216e2656a6b7804c900fd3ebd7e0cb6758f9
Sergii Gatezh <gatezh@users.noreply.github.com>
2022-09-16 14:06:47 -0400

    Add child route to edit a contact card

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Root, {
   action as rootAction,
 } from "./routes/root";
 import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact from "./routes/edit";
 import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
@@ -21,6 +22,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
